d5c1883d89558b54216f7f288f45be5b232904a0
sanjayraoms <sanjayraoms@gmail.com>
2024-02-19 07:52:34 +0530

    Merge Sort my implementaion

--- Sorting/mergeSort.js
+++ Sorting/mergeSort.js
@@ -2,7 +2,7 @@ function merge(arr1,arr2) {
     let mergedarr = [];
     let i = 0;
     let j = 0;
-    while (i < arr1.length && j < arr2.length) { //My solution had or
+    while (i < arr1.length && j < arr2.length) { //My solution had or condition
         // if (i===arr1.length && j < arr2.length){
         //     mergedarr.push(arr2[j]);
         //     j++;
@@ -32,5 +32,11 @@ function merge(arr1,arr2) {
     return mergedarr;
 }
 
+function mergeSort(arr) {
+    if(arr.length === 1) return arr;
+    let mid = Math.floor(arr.length/2);
+    return merge(mergeSort(arr.slice(0,mid)),mergeSort(arr.slice(mid,arr.length)));
+}
+
 //console.log(merge([1,10,15],[2,14,99,100]))
-console.log(merge([],[2,14,99,100]))
+console.log(mergeSort([4,1,7,55,22,9,8]))
