d90cba2e566e85a85008d26bbc81cdc2fd02b2f2
sanjayraoms <sanjayraoms@gmail.com>
2024-02-19 07:35:28 +0530

    Modified slightly to not get confused with conditions

--- Sorting/mergeSort.js
+++ Sorting/mergeSort.js
@@ -2,17 +2,17 @@ function merge(arr1,arr2) {
     let mergedarr = [];
     let i = 0;
     let j = 0;
-    while (i < arr1.length || j < arr2.length) {
-        if (i===arr1.length && j < arr2.length){
-            mergedarr.push(arr2[j]);
-            j++;
-            continue;
-        }
-        if (j===arr2.length && i < arr1.length){
-            mergedarr.push(arr1[i]);
-            i++;
-            continue;
-        }
+    while (i < arr1.length && j < arr2.length) { //My solution had or
+        // if (i===arr1.length && j < arr2.length){
+        //     mergedarr.push(arr2[j]);
+        //     j++;
+        //     continue;
+        // }
+        // if (j===arr2.length && i < arr1.length){
+        //     mergedarr.push(arr1[i]);
+        //     i++;
+        //     continue;
+        // }
         if (arr1[i] < arr2[j]) {
             mergedarr.push(arr1[i]);
             i++;
@@ -21,7 +21,16 @@ function merge(arr1,arr2) {
             j++;
         }
     }
+    while (i<arr1.length) {
+        mergedarr.push(arr1[i]);
+        i++;
+    }
+    while (j<arr2.length) {
+        mergedarr.push(arr2[j]);
+        j++;
+    }
     return mergedarr;
 }
 
-console.log(merge([1,10,15],[2,14,99,100]))
+//console.log(merge([1,10,15],[2,14,99,100]))
+console.log(merge([],[2,14,99,100]))
