84ffaac99d1986106ce76c20560f5cd3f5f6b7e0
sanjayraoms <sanjayraoms@gmail.com>
2024-02-27 22:33:57 +0530

    SSL Insert,Remove and Reverse

--- DataStructures/Singly_Linked_List.js
+++ DataStructures/Singly_Linked_List.js
@@ -104,6 +104,59 @@ class SinglyLinkedList{
         }
         return false;
     }
+    insert(index,val){
+        if(index > this.length && index < 0) return false;
+        if(index === this.length) return !!this.push(val);
+        if(index === 0) return !!this.unshift(val);
+
+        let newNode = new Node(val);
+        let prevNode = this.get(index-1);
+        newNode.next = prevNode.next;
+        prevNode.next = newNode;
+        this.length++;
+        return true;
+    }
+    remove(index){
+        if(index > this.length && index < 0) return false;
+        if(index=== this.length-1) return this.pop();
+        if(index === 0) return this.shift();
+
+        let prevNode = this.get(index-1);
+        let removeNode = prevNode.next;
+        prevNode.next = removeNode.next;
+        this.length--;
+        return removeNode;
+    }
+    reverse(){
+        //My solution
+        // let temp = this.head;
+        // this.head = this.tail;
+        // this.tail = temp;
+        // let prev = this.tail;
+        // let current = prev.next;
+        // let next = current.next;
+        // this.tail.next = null;
+        // while(true){
+        //     current.next = prev;
+        //     prev = current;
+        //     if(prev === this.head) break;
+        //     current = next;
+        //     next = current.next;
+        // }
+        //Better solution
+        var node = this.head;
+        this.head = this.tail;
+        this.tail = node;
+        var next;
+        var prev = null;
+        for (let index = 0; index < this.length; index++) {
+            next = node.next;
+            node.next = prev;
+            prev = node;
+            node = next;
+        }
+        return this;
+    }
 }
 var list= new SinglyLinkedList();
 list.push("Hello");
@@ -114,8 +167,9 @@ list.push("you?");
 // console.log(list.get(0));
 // console.log(list.get(2));
 // console.log(list.get(4));
-console.log(list.set(6,"Where"));
-console.log(list)
+// console.log(list.insert(0,"FIRST"));
+// console.log(list.remove(4));
+console.log(list.reverse());
 // console.log(list.unshift("Hi,"));
 // console.log(list.shift());
 // console.log(list.shift());
