be818c108e04338dc2c0964f52563c37766e088b
sanjayraoms <sanjayraoms@gmail.com>
2024-03-04 23:04:57 +0530

    Doubly linked list get,set and insert methods

--- DataStructures/Doubly_Linked_List.js
+++ DataStructures/Doubly_Linked_List.js
@@ -39,7 +39,7 @@ class DoublyLinkedList{
         this.length--;
         return currentTail;
     }
-    shift(val){
+    unshift(val){
         var newHead = new Node(val);
         if (this.length === 0) {
             this.head = newHead;
@@ -52,18 +52,65 @@ class DoublyLinkedList{
         this.length++;
         return this;
     }
-    unshift(){
+    shift(){
         if(this.length === 0) return undefined;
-        var unshifthead = this.head;
+        var shifthead = this.head;
         if(this.length === 1){
             this.head = null;
             this.tail = null;
         }else{
-            this.head = unshifthead.next;
+            this.head = shifthead.next;
             this.head.prev = null;
+            shifthead.next = null;
         }        
         this.length--;
-        return unshifthead;
+        return shifthead;
+    }
+    get(index){
+        if(index < 0 || index >= this.length) return undefined;
+        if(index === 0) return this.head;
+        var current,count;
+        if (index >= this.length/2) {
+            current = this.tail;
+            count = this.length - 1;
+            while(count !== index){
+                current = current.prev;
+                count--;
+            }
+        }else{
+            count = 0;
+            current = this.head;
+            while(index > 0){
+                current = current.next;
+                count++;
+            }
+        }
+        return current;
+    }
+    set(index,val){
+        var updateNode = this.get(index);
+        if (updateNode !== undefined) {
+            updateNode.val = val;
+            return true;
+        }
+        return false;
+    }
+    insert(index,val){
+        if(index < 0 || index > this.length) return false;
+        if(index == 0) 
+            this.unshift(val);
+        else if(index == this.length)
+            this.push(val);
+        else{
+            var newNode = new Node(val);
+            var prevNode = this.get(index-1);
+            newNode.next = prevNode.next;
+            newNode.prev = prevNode;
+            prevNode.next = newNode;
+            newNode.next.prev = newNode;
+            this.length++;
+        }
+        return list;
     }
 }
 
@@ -71,5 +118,8 @@ var list = new DoublyLinkedList();
 list.push("Hey");
 list.push("How");
 list.push("are");
+list.push("you");
+list.push("?");
 // console.log(list.pop())
-console.log()
+console.log();
+//console.log(list.get(3))
