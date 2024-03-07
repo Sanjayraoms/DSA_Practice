fd569b904227f08b511a5aea3869b89dcb984c78
sanjayraoms <sanjayraoms@gmail.com>
2024-03-07 21:36:24 +0530

    Stack implementation

---
+++ DataStructures/Stack.js
@@ -0,0 +1,41 @@
+class Node{
+    constructor(val){
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class Stack{
+    constructor(){
+        this.First = null;
+        this.Last = null;
+        this.length = 0;
+    }
+    push(val){
+        var newNode = new Node(val);
+        if (this.length === 0) {
+            this.First = newNode;
+            this.Last = newNode;
+        }else{
+            newNode.next = this.First;
+            this.First = newNode;
+        }
+        this.length++;
+        return this;
+    }
+    pop(){
+        if(this.length == 0) return null;
+        var poppedVal = this.First;
+        this.First = poppedVal.next;
+        poppedVal.next = null;
+        this.length--;
+        if (this.length === 0) {
+            this.First = null;
+            this.Last = null;
+        }
+        return poppedVal;
+    }
+}
+var st= new Stack();
+st.push("Hi");
+console.log();
