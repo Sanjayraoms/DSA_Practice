3f1b8820cda4eb7af948b7a7a279a0302a3209f1
sanjayraoms <sanjayraoms@gmail.com>
2024-03-27 05:47:10 +0530

    Practiced Dijktra's algorithm

--- DataStructures/Dijktra'sAlgorithm.js
+++ DataStructures/Dijktra'sAlgorithm.js
@@ -9,13 +9,82 @@ class WeightedGraph{
         this.adjacencyList[vertex1].push({node:vertex2,weight});
         this.adjacencyList[vertex2].push({node:vertex1,weight});
     }
+    Dijktra(start,finish){
+        const nodes = new PriorityQueue();
+        const distances = {};
+        const previous = {};
+        let smallest;
+        var path=[];
+        for (let vertex in this.adjacencyList) {
+            if (vertex === start) {
+                distances[vertex] = 0;
+                nodes.enqueue(vertex,0);
+            }else{
+                distances[vertex] = Infinity;
+                nodes.enqueue(vertex,Infinity);
+            }
+            previous[vertex] = null;
+        }
+        while (nodes.values.length) {
+            smallest = nodes.dequeue().val;
+            if (smallest === finish) {
+                while (previous[smallest] !== null) {
+                    path.push(previous[smallest])
+                    smallest = previous[smallest];
+                }
+                path.reverse();
+                path.push(finish);
+                break;
+            }
+            for (let neighbor in this.adjacencyList[smallest]) {
+                //find neighbouring node
+                let nextNode = this.adjacencyList[smallest][neighbor];
+                let candidate = distances[smallest] + nextNode.weight;
+                let nextNeighbor = nextNode.node;
+                if (candidate < distances[nextNeighbor]) {
+                    distances[nextNeighbor] = candidate;
+                    previous[nextNeighbor] = smallest;
+                    nodes.enqueue(nextNeighbor,candidate);
+                }
+                
+            }
+        }
+        return path;
+    }
+}
+
+class PriorityQueue{
+    constructor(){
+        this.values = [];
+    }
+    enqueue(val,priority){
+        this.values.push({val,priority});
+        this.sort();
+    }
+    dequeue(){
+        return this.values.shift();
+    }
+    sort(){
+        this.values.sort((a,b) => a.priority - b.priority);
+    }
 }
 
 var graph = new WeightedGraph();
 graph.addVertex("A");
 graph.addVertex("B");
 graph.addVertex("C");
-graph.addEdge("A","B",10);
-graph.addEdge("A","C",5);
-graph.addEdge("B","C",7);
+graph.addVertex("D");
+graph.addVertex("E");
+graph.addVertex("F");
+
+graph.addEdge("A","B",4);
+graph.addEdge("A","C",2);
+graph.addEdge("B","E",3);
+graph.addEdge("C","D",2);
+graph.addEdge("C","F",4);
+graph.addEdge("D","E",3);
+graph.addEdge("D","F",1);
+graph.addEdge("E","F",1);
+console.log(graph.Dijktra("A","E"))
+console.log(graph.Dijktra("A","D"))
 console.log
