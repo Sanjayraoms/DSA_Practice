1caee4e71e454b862c54aa238b27e63fddb57250
Sanjay Rao M S <sanjayraoms@gmail.com>
2024-07-27 13:05:21 +0530

    Time: 102 ms (78.84%), Space: 48 MB (90.55%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.cs
+++ 0605-can-place-flowers/0605-can-place-flowers.cs
@@ -4,7 +4,7 @@ public class Solution {
            return true;
         if(flowerbed.Length == 1)
            return (flowerbed[0]==0);
-        for(int i = 0; i < flowerbed.Length; i++){
+        for(int i = 0; i < flowerbed.Length;i++){
             if(i == 0){
                 if(flowerbed[i+1] == 0 && flowerbed[i] == 0){
                     n--;
@@ -13,6 +13,7 @@ public class Solution {
             }else if(i == flowerbed.Length - 1){
                 if(flowerbed[i-1] == 0 && flowerbed[i] == 0)
                    n--;
+                   i++;
             }
             else if(flowerbed[i-1] == 0 &&
                     flowerbed[i] == 0 &&
