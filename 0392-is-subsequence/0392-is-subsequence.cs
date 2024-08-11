e78445056020e559fedb47ca2c92a3e868701e92
Sanjay Rao M S <sanjayraoms@gmail.com>
2024-08-12 05:29:47 +0530

    Time: 56 ms (51.47%), Space: 40 MB (18.7%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.cs
@@ -0,0 +1,20 @@
+public class Solution {
+    public bool IsSubsequence(string s, string t) {
+        if(s.Length == 0)
+          return true;
+        if(s.Length > t.Length)
+           return false;
+        int i = 0;
+        int j = 0;
+        while(i < s.Length && j < t.Length){
+            if(s[i] == t[j]){
+                i++;
+                j++;
+                if(i == s.Length)
+                   return true;
+            }else
+               j++;
+        }
+        return false;
+    }
+}
