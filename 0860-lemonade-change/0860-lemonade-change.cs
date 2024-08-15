b827e284068e28d40d9ea0dd1c1a265685a3a86b
Sanjay Rao M S <sanjayraoms@gmail.com>
2024-08-15 16:21:52 +0530

    Time: 316 ms (79.75%), Space: 57.7 MB (33.74%) - LeetHub

--- 0860-lemonade-change/0860-lemonade-change.cs
+++ 0860-lemonade-change/0860-lemonade-change.cs
@@ -3,28 +3,21 @@ public class Solution {
         int fives = 0;
         int tens = 0;
         foreach(int bill in bills){
-            switch(bill){
-                case 5 :
-                   fives++;
-                   break;
-                case 10 :
-                   if(fives > 0){
-                    tens++;
-                    fives--;
-                   }
-                   else
-                     return false;
-                   break;
-                case 20 :
-                   if(tens > 0 && fives > 0){
+            if(bill == 5)
+               fives++;
+            else if(bill == 10){
+                tens++;
+                fives--;
+            }else{
+                if(tens > 0){
                     tens--;
                     fives--;
-                   }else if(fives > 2)
-                      fives -= 3;
-                    else
-                      return false;
-                   break;
+                }else{
+                    fives -= 3;
+                }
             }
+            if(fives < 0)
+               return false;
         }
         return true;
     }
