e7daadcf0652c217cd9245ac1bc0a063b14ce539
sanjayraoms <sanjayraoms@gmail.com>
2024-09-04 11:54:10 +0530

    Max Binary Heap Insert

---
+++ DSA_C#/DSA/BinaryHeap/MaxBinaryHeap.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryHeap
+{
+    public class MaxBinaryHeap
+    {
+        List<int> heap = new List<int>();
+
+        public int[] InsertItem(int val)
+        {
+            heap.Add(val);
+            bubbleUp();
+            return heap.ToArray();
+        }
+        
+        private void bubbleUp()
+        {
+            var currentIndex = heap.Count - 1;
+            while (currentIndex > 0)
+            {
+                var parentIndex = (int)Math.Floor((double)((currentIndex - 1) / 2));
+                if (heap[currentIndex] > heap[parentIndex])
+                {
+                    var temp = heap[parentIndex];
+                    heap[parentIndex] = heap[currentIndex];
+                    heap[currentIndex] = temp;
+                    currentIndex = parentIndex;
+                }
+                else
+                    return;
+            }
+
+        }
+    }
+}
---
+++ DSA_C#/DSA/BinaryHeap/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BinaryHeap;
+
+Console.WriteLine("Hello, World!");
+
+MaxBinaryHeap heap = new MaxBinaryHeap();
+heap.InsertItem(10);
+heap.InsertItem(20).ToList().ForEach(x => Console.Write($"{x} "));
+Console.WriteLine();
+heap.InsertItem(30).ToList().ForEach(x => Console.Write($"{x} "));
+Console.WriteLine();
+heap.InsertItem(40).ToList().ForEach(x => Console.Write($"{x} "));
+Console.WriteLine();
+heap.InsertItem(15).ToList().ForEach(x => Console.Write($"{x} "));
+Console.WriteLine();
+heap.InsertItem(50).ToList().ForEach(x=> Console.Write($"{x} "));
+
+
