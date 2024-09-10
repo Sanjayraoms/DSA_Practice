2bba8def9621526cd8d504812b72d3160fa03308
sanjayraoms <sanjayraoms@gmail.com>
2024-09-10 05:32:03 +0530

    Adding vertex and edges

---
+++ DSA_C#/DSA/Graph/Graph.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graphs
+{
+    public class Graph
+    {
+        IDictionary<int, List<int>> adjacencyList;
+
+        public Graph()
+        {
+            adjacencyList = new Dictionary<int, List<int>>();
+        }
+
+        public void AddVertex(int vertex)
+        {
+            adjacencyList[vertex] = new List<int>();
+        }
+
+        public void AddEdge(int vertex1,int vertex2)
+        {
+            adjacencyList[vertex1].Add(vertex2);
+            adjacencyList[vertex2].Add(vertex1);
+        }
+    }
+}
---
+++ DSA_C#/DSA/Graph/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Graphs;
+
+Console.WriteLine("Hello, World!");
+
+var g = new Graph();
+g.AddVertex(2);
+g.AddVertex(3);
+g.AddVertex(5);
+g.AddVertex(6);
+g.AddVertex(7);
+
+g.AddEdge(2, 3);
+g.AddEdge(2, 7);
+g.AddEdge(3, 5);
+g.AddEdge(5, 6);
+g.AddEdge(6, 7);
+
+
+
