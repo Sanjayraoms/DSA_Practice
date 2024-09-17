fd49839eaede76160e058c912c73f3c8d0aa64a9
Sanjay Rao M S <sanjayraoms@gmail.com>
2024-09-17 21:24:49 +0530

    Time: 111 ms (12.96%), Space: 46.8 MB (84.26%) - LeetHub

--- 0884-uncommon-words-from-two-sentences/0884-uncommon-words-from-two-sentences.cs
+++ 0884-uncommon-words-from-two-sentences/0884-uncommon-words-from-two-sentences.cs
@@ -1,7 +1,6 @@
 public class Solution {
     public string[] UncommonFromSentences(string s1, string s2) {
         Dictionary<string,int> str1Ctr = new Dictionary<string,int>();
-        List<string> uncommon = new List<string>();
         foreach(string str in s1.Split(' ')){
             if(!str1Ctr.ContainsKey(str))
                str1Ctr[str] = 1;
@@ -16,10 +15,6 @@ public class Solution {
                 str1Ctr[str]++;
             }
         }
-        foreach(string key in str1Ctr.Keys){
-            if(str1Ctr[key] == 1)
-               uncommon.Add(key);
-        }
-        return uncommon.ToArray();
+        return str1Ctr.Where(x => x.Value==1).Select(k=>k.Key).ToArray();
     }
 }
