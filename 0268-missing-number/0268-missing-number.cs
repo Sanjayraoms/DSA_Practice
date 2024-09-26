e6f0536cb16bdafcc03fce23fdc9e53ee70bcb68
Sanjay Rao M S <sanjayraoms@gmail.com>
2024-09-27 05:29:24 +0530

    Time: 100 ms (57.86%), Space: 45.9 MB (44.06%) - LeetHub

--- 0268-missing-number/0268-missing-number.cs
+++ 0268-missing-number/0268-missing-number.cs
@@ -1,10 +1,16 @@
 public class Solution {
     public int MissingNumber(int[] nums) {
-        HashSet<int> distnums = new HashSet<int>(nums);
-        for(int i = 0; i<=nums.Length;i++){
-            if(!nums.Contains(i))
+        // HashSet<int> distnums = new HashSet<int>(nums);
+        // for(int i = 0; i<=nums.Length;i++){
+        //     if(!nums.Contains(i))
+        //         return i;
+        // }
+        // return 0;
+        Array.Sort(nums);
+        for(int i = 0; i< nums.Length;i++){
+            if(i!=nums[i])
                 return i;
         }
-        return 0;
+        return nums.Length;
     }
 }
