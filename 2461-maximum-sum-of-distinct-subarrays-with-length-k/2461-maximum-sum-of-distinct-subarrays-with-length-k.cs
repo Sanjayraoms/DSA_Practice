09d8d1855a3c51d8f46909eedf47ad80bdd77bba
Sanjay Rao M S <sanjayraoms@gmail.com>
2024-11-23 06:11:00 +0530

    Time: 23 ms (45.8%), Space: 70.5 MB (49.77%) - LeetHub

--- 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.cs
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.cs
@@ -1,55 +1,31 @@
 public class Solution {
     public long MaximumSubarraySum(int[] nums, int k)
         {
-            int start = 0;
-            int end = 0;
             Dictionary<int, int> checkDup = new Dictionary<int, int>();
             long max = 0;
-            long sum = 0;
-            while (start <= nums.Length - k)
-            {
-                end = start + k - 1;
-                for (int i = start; i <= end; i++)
-                {
-                    if (checkDup.ContainsKey(nums[i]))
-                    {
-                        checkDup.Clear();
-                        max = 0;
-                        if (nums[i] == nums[i - 1]) start = i;
-                        else start++;
-                        break;
-                    }
-                    else
-                    {
-                        checkDup[nums[i]] = 1;
-                        max += nums[i];
-                    }
-                }
-                if (max > 0)
-                    break;
-            }
-            if (max == 0)
-                return 0;
-            sum = max;
-            end++;
-            for (int i = end; i < nums.Length; i++)
-            {
-                checkDup[nums[start]]--;
-                if (checkDup[nums[start]] == 0)
-                    checkDup.Remove(nums[start]);
-                sum -= nums[start];
-                sum += nums[i];
-                if (!checkDup.ContainsKey(nums[i]))
-                {
-                    checkDup[nums[i]] = 1;
-                    if(checkDup.Count == k)
-                        max = Math.Max(max, sum);
-                }
-                else
-                {
+            long currSum = 0;
+            for(int i = 0; i < k ; i++){
+                currSum+=nums[i];
+                if(checkDup.ContainsKey(nums[i]))
                     checkDup[nums[i]]++;
-                }                
-                start++;
+                else
+                    checkDup[nums[i]] = 1;
+            }
+            if(checkDup.Count == k)
+                max = currSum;
+            for(int i = 1; i <= nums.Length - k;i++){
+                checkDup[nums[i-1]]--;
+                currSum -= nums[i-1];
+                if(checkDup[nums[i-1]] == 0)
+                    checkDup.Remove(nums[i-1]);
+                if(checkDup.ContainsKey(nums[i+k-1])){
+                    checkDup[nums[i+k-1]]++;
+                }else{
+                    checkDup[nums[i+k-1]] = 1;
+                }
+                currSum += nums[i+k-1];
+                if(checkDup.Count == k)
+                    max = Math.Max(max,currSum);
             }
             return max;
         }
