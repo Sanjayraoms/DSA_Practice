c4141e064b25dce75722ad6370d695e7b3fbc3ea
Sanjay Rao M S <sanjayraoms@gmail.com>
2024-12-15 22:28:07 +0530

    Time: 1257 ms (14.29%), Space: 81.8 MB (71.43%) - LeetHub

---
+++ 1792-maximum-average-pass-ratio/1792-maximum-average-pass-ratio.cs
@@ -0,0 +1,30 @@
+public class Solution {
+    public double MaxAverageRatio(int[][] classes, int extraStudents) {
+        var pq = new PriorityQueue<int[],double>();
+        double avg = 0;
+        double res = 0;
+        foreach(var cls in classes){
+            avg = profit(cls[0],cls[1]);
+            pq.Enqueue(cls,avg);
+        }
+        while(extraStudents > 0){
+            var dq = pq.Dequeue();
+            dq[0]++;
+            dq[1]++;
+            avg = profit(dq[0],dq[1]);
+            pq.Enqueue(dq,avg);
+            extraStudents--;
+        }
+        while(pq.Count > 0){
+            var dq = pq.Dequeue();
+            avg = (double)dq[0]/dq[1];
+            res += avg;
+        }
+        return res/classes.Length;
+    }
+    public double profit(int i,int j){
+        var oldavg = (double)i++/j++;
+        var newavg = (double)i/j;
+        return oldavg - newavg;
+    }
+}
