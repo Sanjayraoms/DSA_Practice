5f3db4ceedcca8d78786a5a81c3acd6f0a7bb173
Sanjay Rao M S <sanjayraoms@gmail.com>
2024-12-19 12:26:31 +0530

    Time: 1 ms (6.25%), Space: 40 MB (62.5%) - LeetHub

---
+++ 0769-max-chunks-to-make-sorted/0769-max-chunks-to-make-sorted.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public int MaxChunksToSorted(int[] arr) {
+        Stack<int> st = new Stack<int>();
+        st.Push(arr[0]);
+        for(int i = 1; i < arr.Length; i++){
+            if(arr[i] > st.Peek()){
+                st.Push(arr[i]);
+            }else{
+                var currMax = st.Peek();
+                while(st.Count > 0 && arr[i] < st.Peek()){
+                    st.Pop();
+                }
+                st.Push(currMax);
+            }
+        }
+        return st.Count;
+    }
+}
