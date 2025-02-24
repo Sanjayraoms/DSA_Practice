85aab1d1c483c078e2d740f388f54fafea9aae32
Sanjay Rao M S <sanjayraoms@gmail.com>
2025-02-25 05:28:54 +0530

    Time: 4 ms (19.66%), Space: 44.9 MB (9.89%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.cs
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.cs
@@ -1,9 +1,10 @@
 public class Solution {
     public string FindDifferentBinaryString(string[] nums) {
-        int k = nums[0].Length;
+        int k = nums[0].Length - 1;
         double len = 0;
-        while(k-- >= 0){
+        while(k >= 0){
             len = len + Math.Pow(2,k);
+            k--;
         }
         int[] allPossible = new int[(int)len+1];
         k = nums[0].Length;
@@ -14,8 +15,7 @@ public class Solution {
         for(int i = 0; i < allPossible.Length; i++){
             if(allPossible[i] == 0){
                 var res = Convert.ToString(i,2);
-                var pad = k - res.Length + 1;
-                return res.PadLeft(pad,'0');
+                return res.PadLeft(k,'0');
             }
         }
         return "";
