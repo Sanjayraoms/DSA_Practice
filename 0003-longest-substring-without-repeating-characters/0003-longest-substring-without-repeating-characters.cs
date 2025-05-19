e15a39cd9dc38c3855900745cdd8f50c03e29783
Sanjay Rao M S <sanjayraoms@gmail.com>
2025-05-19 06:43:52 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cs
@@ -0,0 +1,25 @@
+public class Solution {
+    public int LengthOfLongestSubstring(string s) {
+        var set = new HashSet<char>();
+        var max = 0;
+        var sb =  new StringBuilder("");
+
+        for(int i = 0; i < s.Length; i++){
+            if(!set.Add(s[i])){
+                var indx = 0;
+                while(indx < sb.Length){
+                    if(sb[indx] == s[i]){
+                        sb.Remove(0,indx);
+                        break;
+                    }
+                    set.Remove(sb[indx]);
+                    indx++;
+                }
+            }
+            set.Add(s[i]);
+            sb.Append(s[i]);
+            max = Math.Max(max, set.Count);
+        }
+        return max;
+    }
+}
