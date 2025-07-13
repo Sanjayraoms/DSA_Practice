ef20bafe2f0a169e6ca25b19e720475905456985
sanjayraoms <sanjayraoms@gmail.com>
2025-07-13 06:31:02 +0530

    Basic recursion problems

---
+++ DSA_C#/DSA/Recursion/BaseConditionHypothesisInductionPattern.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursion
+{
+    public class BaseConditionHypothesisInductionPattern
+    {
+        public void Print1tonRecursively(int n)
+        {
+            //Base Condition
+            if(n == 1)
+            {
+                Console.WriteLine(n);
+                return;
+            }
+            //Hypothesis and induction
+            Print1tonRecursively(n - 1);
+            Console.WriteLine(n);
+        }
+
+        public void Printnto1Recursively(int n)
+        {
+            //Base Condition
+            if (n == 1)
+            {
+                Console.WriteLine(n);
+                return;
+            }
+            //Hypothesis and induction
+            Console.WriteLine(n);
+            Printnto1Recursively(n - 1);
+        }
+
+        public int Factorial(int n)
+        {
+            if(n == 1)
+                return 1;
+            return n * Factorial(n - 1);
+        }
+    }
+}
---
+++ DSA_C#/DSA/Recursion/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Recursion;
+
+Console.WriteLine("Hello, World!");
+
+var test = new BaseConditionHypothesisInductionPattern();
+Console.WriteLine($"Factorial of 5 {test.Factorial(5)}"); 
