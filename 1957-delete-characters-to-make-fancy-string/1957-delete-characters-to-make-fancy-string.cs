bea21130afb675cc75719fa547713babb9ef50d1
Sanjay Rao M S <sanjayraoms@gmail.com>
2025-07-21 19:19:18 +0530

    Time: 10 ms (90%), Space: 49.5 MB (56.67%) - LeetHub

--- 1957-delete-characters-to-make-fancy-string/1957-delete-characters-to-make-fancy-string.cs
+++ 1957-delete-characters-to-make-fancy-string/1957-delete-characters-to-make-fancy-string.cs
@@ -1,11 +1,13 @@
 public class Solution {
     public string MakeFancyString(string s) {
-        char[] chrArr = s.ToCharArray();
-        for(int i = 1; i < chrArr.Length - 1; i++){
-            if(chrArr[i-1] == chrArr[i] && chrArr[i] == chrArr[i+1]){
-                chrArr[i-1] = ' ';
-            }
+        var res = new StringBuilder("");
+        res.Append(s[0]);
+        for(int i = 1; i < s.Length - 1; i++){
+            if(s[i] != s[i-1] || s[i] != s[i+1])
+                res.Append(s[i]);
         }
-        return new string(new string(chrArr).Where(x => !char.IsWhiteSpace(x)).ToArray());
+        if(s.Length > 1)
+            res.Append(s[^1]);
+        return res.ToString();
     }
 }
