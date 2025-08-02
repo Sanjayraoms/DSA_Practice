77cdf4b7e6b50ca0a6781a6758c0ed85b40b79f0
Sanjay Rao M S <sanjayraoms@gmail.com>
2025-08-02 23:17:55 +0530

    Time: 0 ms (100%), Space: 30.8 MB (64.71%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public bool IsPowerOfFour(int n) {
+        if(n <= 0)
+            return false;
+        while(n != 1)
+        {
+            if(n%4 != 0)
+            {
+                return false;
+            }
+            n = n/4;
+        }
+        return true;
+    }
+}
