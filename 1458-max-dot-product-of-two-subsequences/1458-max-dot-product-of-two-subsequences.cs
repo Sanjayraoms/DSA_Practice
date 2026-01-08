47044e087a0768444bed457f12568be01967548b
Sanjay Rao M S <sanjayraoms@gmail.com>
2026-01-08 22:08:47 +0530

    Time: 11 ms (0%), Space: 45.1 MB (28.57%) - LeetHub

---
+++ 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public int MaxDotProduct(int[] nums1, int[] nums2) {
+        int n = nums1.Length, m = nums2.Length;
+        int NEG = (int)-1e9;
+        int[,] dp = new int[n+1,m+1];
+
+        for(int i = 0;i <= n;i++)
+            for(int j = 0; j<=m;j++)
+                dp[i,j] = NEG;
+        
+        for(int i = 1; i <=n; i++){
+            for(int j = 1; j <=m; j++){
+                int take = nums1[i-1]*nums2[j-1] + Math.Max(0,dp[i-1,j-1]);
+                dp[i,j] = Math.Max(take,Math.Max(dp[i-1,j],dp[i,j-1]));
+            }
+        }
+        return dp[n,m];
+    }
+}
