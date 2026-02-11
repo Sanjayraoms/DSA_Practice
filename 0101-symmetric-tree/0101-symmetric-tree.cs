b2cf04304575ea1ee98829afe50fc2635dc0a4ce
Sanjay Rao M S <sanjayraoms@gmail.com>
2026-02-11 21:55:29 +0530

    Time: 0 ms (100%), Space: 44.3 MB (87.51%) - LeetHub

---
+++ 0101-symmetric-tree/0101-symmetric-tree.cs
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public bool IsSymmetric(TreeNode root) {
+        return checkSymmetric(root.left,root.right);
+    }
+    private bool checkSymmetric(TreeNode node1, TreeNode node2){
+        if(node1 == null && node2 == null)
+            return true;
+        if(node1 == null || node2 == null)
+            return false;
+        return (node1.val == node2.val) 
+               && checkSymmetric(node1.left, node2.right)
+               && checkSymmetric(node1.right, node2.left);
+    }
+}
